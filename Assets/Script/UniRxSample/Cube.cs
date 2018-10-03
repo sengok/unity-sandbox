9f45034c9b9dd979ab4f1cbd8f10e8443b432f79
Wataru Sendo <sendo_wataru@cyberagent.co.jp>
2018-10-04 01:13:37 +0900

    UniRx追加, オブジェクト動かして遊んだ

---
+++ Assets/Script/UniRxSample/Cube.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class Cube : MonoBehaviour
+{
+
+    public float durationSecond = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.UpdateAsObservable()
+            .Take(TimeSpan.FromSeconds(durationSecond))
+            .Subscribe(_ =>
+            {
+                this.gameObject.transform.Translate(Vector3.right * Time.deltaTime);
+            })
+            .AddTo(this);
+    }
+}
